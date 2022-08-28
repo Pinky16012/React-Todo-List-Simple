5579b7f39af4b9d7d774860431799aa5f0d91bff
COMPAL\Pinky_Shen <kyis8804@gmail.com>
2022-08-28 14:51:50 +0800

    feature: Add Default Setting & Router Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import TodoList from "./page/todoList/todoList";
+import SignUp from "./page/signup/signup";
+import Login from "./page/login/login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="todolist" element={<TodoList />} />
+      </Routes>
     </div>
   );
 }
